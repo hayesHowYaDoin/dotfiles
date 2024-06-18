d1d34d340908a8f4122f7ffe323eeddf5551ec6c
Jordan Hayes <jordanhayes98@gmail.com>
2024-06-17 21:16:37 -0600

    Added kitty and fontconfig

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# HISTFILE="$XDG_DATA_HOME"/zsh/history
 HISTSIZE=1000000
 SAVEHIST=1000000
 
 export EDITOR="nvim"
+export TERMINAL="kitty"
 export PATH="$HOME/.local/bin":$PATH
 export PATH="$HOME/.docker/bin":$PATH
 export MANPAGER='nvim +Man!'
