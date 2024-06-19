0c8f00be206bba5884e87f2dbfb75358e89562cf
Jordan Hayes <jordanhayes98@gmail.com>
2024-06-19 08:40:52 -0600

    Added setup script for docker dev containers

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Setup file for docker dev containers extension for VS Code
+
+sudo apt-get install stow && stow zsh
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -2,6 +2,7 @@
 
 LC_CTYPE=en_US.UTF-8
 LC_ALL=en_US.UTF-8
+
 # History
 HISTFILE=~/.zsh_history
 
