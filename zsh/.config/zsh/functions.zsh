53b436e49243394365c4fef5de597fcdcd179916
Jordan Hayes <jordanhayes98@gmail.com>
2024-06-21 10:54:36 -0600

    Added conditional creation of ~/.local/bin directory

--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -8,6 +8,7 @@ install_dependencies() {
     fi
 
     if [[ ! $(command -v oh-my-posh) ]] then
+        mkdir -p ~/.local/bin
         curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin
     fi
 }
