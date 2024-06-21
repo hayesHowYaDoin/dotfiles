330c51e206ebf73e2adab94bd065eb94901a5824
Jordan Hayes <jordanhayes98@gmail.com>
2024-06-21 11:06:24 -0600

    Stow with adopt flag and then git restore to get around dev containers with existing .zshrc

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # Setup file for docker dev containers extension for VS Code
 
-sudo apt-get install stow && stow zsh
-zsh # Start to prompt to download dependencies
+sudo apt-get install stow
+
+# Take current files, then overwrite them
+stow --adopt zsh
+git restore zsh
+
+# Start to prompt to download dependencies
+zsh
