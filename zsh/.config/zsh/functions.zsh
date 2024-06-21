c4501547f24f2c94d87bfbcb3ff8ce89d49dff37
Jordan Hayes <jordanhayes98@gmail.com>
2024-06-21 10:51:51 -0600

    Re-added unzip as a dependency of zsh

--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -3,6 +3,10 @@ install_dependencies() {
         sudo apt-get install curl
     fi
 
+    if [[ ! $(command -v unzip) ]] then
+        sudo apt-get install unzip
+    fi
+
     if [[ ! $(command -v oh-my-posh) ]] then
         curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin
     fi
