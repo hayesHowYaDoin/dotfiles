64e04462c2d6796d5cc5eecd43afb9e6d7ba8500
Jordan Hayes <jhayes5@ncsu.edu>
2024-08-02 10:59:00 -0600

    Swapped around case where sudo is used

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@
 # Setup file for docker dev containers extension for VS Code
 
 if [[ "$EUID" -ne 0 ]]; then
-  apt-get install stow
-else
   sudo apt-get install stow
+else
+  apt-get install stow
 fi
 
 stow zsh
