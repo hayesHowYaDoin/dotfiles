7ed85e5d3d6846d0d083e3471a7d141f6bf380b2
Jordan Hayes <jhayes5@ncsu.edu>
2024-08-02 11:13:59 -0600

    Assign sudo command to variable to avoid repeating commands

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Setup file for docker dev containers extension for VS Code
 
+SUDO=""
 if [[ "$EUID" -ne 0 ]]; then
-  sudo apt-get update
-  sudo apt-get install stow
-else
-  apt-get update
-  apt-get install stow
+    SUDO="sudo"
 fi
+
+$SUDO apt-get update
+$SUDO apt-get install stow
 
 stow zsh
