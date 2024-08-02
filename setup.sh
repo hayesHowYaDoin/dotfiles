1a3c5da5d8f3bae0c424e6630117cf8211b4b0bd
Jordan Hayes <jhayes5@ncsu.edu>
2024-08-02 11:08:19 -0600

    Added command to apt-get update

--- setup.sh
+++ setup.sh
@@ -2,8 +2,10 @@
 # Setup file for docker dev containers extension for VS Code
 
 if [[ "$EUID" -ne 0 ]]; then
+  sudo apt-get update
   sudo apt-get install stow
 else
+  apt-get update
   apt-get install stow
 fi
 
