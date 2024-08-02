49fc036d37426d5119425c6a96128ba6b3f62c59
Jordan Hayes <jhayes5@ncsu.edu>
2024-08-02 10:52:43 -0600

    Updated setup script to not use sudo if running as root

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # Setup file for docker dev containers extension for VS Code
 
-sudo apt-get install stow
+if [[ "$EUID" -ne 0 ]]; then
+  apt-get install stow
+else
+  sudo apt-get install stow
+fi
 
 stow zsh
-
